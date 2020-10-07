e6fc27ea98d4302513851617bf0ec786d64b0f44
Lindsey <linds.fonnes@gmail.com>
2020-10-06 18:09:47 -0600

    rock paper scissors instructions fixed

--- index.js
+++ index.js
@@ -152,16 +152,14 @@ function hungryDog(weight, age){
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
-
 // Rock, Paper, Scissors - Let's play against the computer!
 /*
+Create a global variable that randomly generates the computer's choice
+Use Math.random to determine the computers choice (Math.random gives a random number between 0 and 1)
 Use the game function below to do the following:
-  1. Receive a string that represents the user's choice (either "rock", "paper", or "scissors")
-  2. Use Math.random to determine the computers choice (Math.random gives a random number between 0 and 1)
-  3. Return whether the user won, lost, or tied based on these rules of the game described below
-  
+  1. Receive 2 parameters the user's choice and the computer's choice
+  2. Return whether the user won, lost, or tied based on these rules of the game described below
   RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
-  
   HINT: While you can complete this with only conditionals based on strings, it may help to equate choice to a number when using Math.random()
 */
 
